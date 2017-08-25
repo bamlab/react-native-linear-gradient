854508d53732fa851b25e3e887a8f8d119a995e7
maximesraiki <maximes@theodo.fr>
2017-08-25 13:15:13 +0200

    Add conditionnal import of react

--- BVLinearGradient/BVLinearGradientManager.m
+++ BVLinearGradient/BVLinearGradientManager.m
@@ -1,6 +1,14 @@
 #import "BVLinearGradientManager.h"
 #import "BVLinearGradient.h"
+
+#if __has_include(<React/RCTBridge.h>)
 #import <React/RCTBridge.h>
+#elif __has_include("RCTBridge.h")
+#import "RCTBridge.h"
+#else
+#import "React/RCTBridge.h"   // Required when used as a Pod in a Swift project
+#endif
+
 
 @implementation BVLinearGradientManager
 
